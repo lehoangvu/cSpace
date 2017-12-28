bb341e4d591f36d95d97333ce4361c9d50c160ea
lehoangvu <hoangvu171819@gmail.com>
2017-12-28 15:49:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 console.log('Hello, I\'m supper man in here')
 console.log('Now, I will build your earth')
 
+const { exec } = require('child_process');
+function run(cmd) {
+  exec('cmd', (error, stdout, stderr) => {
+    if (error) {
+      console.error('error');
+      console.error(error);
+      return;
+    }
+    console.log('success')
+    console.log(stdout)
+  });
+}
+
+run('mkdir package')
+run('cd package && npm install jquery')
+run('cd package && ls')
