5e0cc06b410f5a373cc0342958a5f13a97995e1e
lehoangvu <hoangvu171819@gmail.com>
2017-12-28 16:44:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-console.log('Hello, I\'m supper man in here')
-console.log('Now, I will build your earth')
+const express = require('express')
+const app = express()
 
-const { exec } = require('child_process');
-function run(cmd) {
-  exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-      console.error('error');
-      console.error(error);
-      return;
-    }
-    console.log('success')
-    console.log(stdout)
-  });
-}
+app.get('/', (req, res) => res.send('Hello World!'))
 
-//run('mkdir package')
-run('npm install jquery')
-run('firebase deploy --token "1/O-BMOMmPKMEQLSJmUyIA--l1knsCr94HerUmCZAjoEY"')
+app.listen(3000, () => {
+
+  console.log('Hello, I\'m supper man in here')
+  console.log('Now, I will build your earth')
+
+  const { exec } = require('child_process');
+  function run(cmd) {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error('error');
+        console.error(error);
+        return;
+      }
+      console.log('success')
+      console.log(stdout)
+      res.send('success')
+      res.send(stdout)
+    });
+  }
+
+  //run('mkdir package')
+  run('npm install jquery')
+  run('firebase deploy --token "1/O-BMOMmPKMEQLSJmUyIA--l1knsCr94HerUmCZAjoEY"')
+
+
+})
+
+
